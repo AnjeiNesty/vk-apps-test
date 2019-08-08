47c9fd8745c004dd3037b9ac430391e7bbea7967
promo4ka <moroz-yra@yandex.ru>
2019-08-08 14:57:35 +0300

    Updates

---
+++ precache-manifest.6f828046b21d1c4b6bb4aa646092ad45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-apps-test/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "58f68bf6307d766efa7f",
+    "url": "/vk-apps-test/static/js/runtime~main.2ae09b44.js"
+  },
+  {
+    "revision": "228f45269746b21ffa65",
+    "url": "/vk-apps-test/static/js/main.06ae7b79.chunk.js"
+  },
+  {
+    "revision": "25393b45c03cc0838644",
+    "url": "/vk-apps-test/static/js/2.2272502c.chunk.js"
+  },
+  {
+    "revision": "228f45269746b21ffa65",
+    "url": "/vk-apps-test/static/css/main.dd40b554.chunk.css"
+  },
+  {
+    "revision": "25393b45c03cc0838644",
+    "url": "/vk-apps-test/static/css/2.e7ab065c.chunk.css"
+  },
+  {
+    "revision": "1ddb814cdcc0a3b48faa984cdde84615",
+    "url": "/vk-apps-test/index.html"
+  }
+];
